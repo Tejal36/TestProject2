0f2d185c7c99b1a06d98b3331fcbe65d72945c7c
Dinesh <dineshj6311@gmail.com>
2025-01-31 01:41:43 +0530

    Added another print statement

--- TestGitProject/src/tenGitPackage/TestGitClass.java
+++ TestGitProject/src/tenGitPackage/TestGitClass.java
@@ -5,7 +5,7 @@ public class TestGitClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Git");
-		System.out.println("Hello git");
+		System.out.println("Hello Command line");
 
 	}
 
